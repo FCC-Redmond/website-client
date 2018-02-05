4d5c7b1333d1b56060bcabc7fca4468bdb4ad6d5
amgranad <ambergkim@gmail.com>
2018-02-04 20:22:40 -0800

    fetch & add member

--- models/members.js
+++ models/members.js
@@ -4,7 +4,8 @@ var app = app || {};
 
 (module => {
 
-  const _API_URL_ = 'https://localhost:3000';
+  const _API_URL_ = 'http://localhost:3000';
+  // const _API_URL_ = 'https://fccredmond.herokuapp.com';
 
   function Member() { }
 
@@ -12,10 +13,11 @@ var app = app || {};
     console.error(err);
   }
 
-  Member.fetchAll = (callback) => $.getJSON(_API_URL_ + '/api/v0/members/list').then(data => {
-    console.log('data ', data);
-    callback(data)
-  }).catch(errorCallback);
+  Member.fetchAll = () => $.getJSON(_API_URL_ + '/api/v0/members/list').then(data => console.log(data)).catch(err => console.error(errorCallback(err)));
+
+  Member.fetchMember = (lName) => $.getJSON(_API_URL_ + '/api/v0/member' + lName).then(data => console.log(data)).catch(err => console.error(errorCallback(err)));
+
+  Member.addMember = (member) => $.post(_API_URL_ + '/api/v0/member', member).catch(err => console.error(errorCallback(err)));
 
   module.Member = Member;
   
