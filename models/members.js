b3fb9af08e3689ee2691fc3c2c9d83468f5174e6
amgranad <ambergkim@gmail.com>
2018-02-09 14:00:01 -0800

    rewrote addMember

--- models/members.js
+++ models/members.js
@@ -4,8 +4,8 @@ var app = app || {};
 
 (module => {
 
-  // const _API_URL_ = 'http://localhost:3000';
-  const _API_URL_ = 'https://fccredmond.herokuapp.com';
+  const _API_URL_ = 'http://localhost:3000';
+  // const _API_URL_ = 'https://fccredmond.herokuapp.com';
 
   function Member() { }
 
@@ -17,7 +17,23 @@ var app = app || {};
 
   Member.fetchMember = (lName) => $.getJSON(_API_URL_ + '/api/v0/members/' + lName).then(data => console.log(data)).catch(err => console.error(errorCallback(err)));
 
-  Member.addMember = (member) => $.post(_API_URL_ + '/api/v0/members/add', member).catch(err => console.error(errorCallback(err)));
+  Member.addMember = (member) => $.ajax(
+    {
+      "async": true,
+      "crossDomain": true,
+      "url": _API_URL_ + "/api/v0/members/add",
+      "method": "POST",
+      "headers": {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+        "Postman-Token": "6f60d89d-34a3-8bfb-c2a6-9b4a9c6990b9"
+      },
+      "processData": false,
+      "data": JSON.stringify(member)
+    }
+  ).done(function (response) {
+    console.log(response);
+  });
 
   Member.getSkill = (skill) => $.getJSON(_API_URL_ + '/api/v0/members/' + skill).then(data => console.log(data)).catch(err => console.error(errorCallback(err)));
 
