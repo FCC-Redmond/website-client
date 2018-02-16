d50cbf27115bf319abb68f85e8d38c63a56914ac
Apoorva Teja <apoorva.teja@gmail.com>
2018-02-15 19:26:41 -0800

    commit

--- views/members-view.js
+++ views/members-view.js
@@ -17,19 +17,40 @@ var app = app || {};
 
     $("#member-display").append(
       `
-      <h2>${member.firstName} ${member.lastName}</h2>
-      <ul>
-        <li>Skills: ${skills}</li>
-        <li>LinkedIn URL: ${member.linkedInUrl}</li>
-        <li>GitHub URL: ${member.gitHubUrl}</li>
-        <li>Profile URL: ${member.profileUrl}</li>
-        <li>Email: ${member.email}</li>
+      <h2><div name="firstName">${member.firstName}</div><div name="lastName">${member.lastName}</div></h2>
+      <ul id="${member._id}">
+        <li name="skills">Skills: ${skills}</li>
+        <li name="linkedInUrl">LinkedIn URL: ${member.linkedInUrl}</li>
+        <li name="gitHubUrl">GitHub URL: ${member.gitHubUrl}</li>
+        <li name="profileUrl">Profile URL: ${member.profileUrl}</li>
+        <li name="email">Email: ${member.email}</li>
       </ul>
-      <button name="delete" id="${member._id}">Delete</button>
+      <button name="delete" value="${member._id}">Delete</button>
+      <button name="update" value="${member._id}">Update</button>
       `
     );
   }
 
+  memberView.displayUpdateMember = (id) => {
+    let member = {};
+    $('#' + id).children('h2,li').each((idx) => {
+      console.log($(this).data['name']);
+    });
+    $('#' + id).empty();
+    $('#' + id).append(
+      `
+      <h2><input type="text" value="${member.firstName}">&nbsp;<input type="text" value="${member.lastName}"></h2>
+        <li>Skills:<input type="text" value="${member.skills}"></li>
+        <li>LinkedIn URL:<input type="text" value="${member.linkedInUrl}"></li>
+        <li>GitHub URL: <input type="text" value="${member.gitHubUrl}"></li>
+        <li>Profile URL: <input type="text" value="${member.profileUrl}"></li>
+        <li>Email:<input type="text" value="${member.email}"></li>
+      </ul>
+      <button name="delete" value="${member._id}">Delete</button>
+      <button name="update" value="${member._id}">Update</button>
+      `
+    );
+  };
   memberView.displayMultiple = (memberArr) => {
     $("#member-display").empty();
     let member = memberArr.data;
@@ -45,11 +66,17 @@ var app = app || {};
   }
 
   $("#member-display").on('click', 'button', (event) => {
-    let id = event.target.id;
-    app.Member.delete(id, (data) => {
-      console.log(data);
-      memberView.init();
-    });
+    let name = event.target.name;
+    let id = event.target.value;
+    switch (name) {
+      case "delete": app.Member.delete(id, (data) => {
+        console.log(data);
+        memberView.init();
+      });
+        break;
+      case "update": memberView.displayUpdateMember(id); break;
+      default: break;
+    }
   });
 
   $("#skill-input").change(() => {
